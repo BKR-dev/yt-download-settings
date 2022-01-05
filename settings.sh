0f54e858942a2cbeda2ae901d60bba40a4ce0734
BookerVII <26764459+BookerVII@users.noreply.github.com>
2022-01-05 11:13:59 +0100

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,2 +1,2 @@
-yt-download-audio='youtube-dl -f "bestaudio[ext=m4a]" $2'
-yt-download-video='youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" --merge-output-format mp4 $2'
+alias yt-download-audio='youtube-dl -f "bestaudio[ext=m4a]" $2'
+alias yt-download-video='youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" --merge-output-format mp4 $2'
